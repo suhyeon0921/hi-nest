931ae4613d9ffd3820bb82a6deab2f7ccac06b06
suhyeon <suhyeon3484@naver.com>
2023-06-04 20:29:03 +0900

    feat: swagger 연동

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from 'src/util/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
   // 유효성 검사
   app.useGlobalPipes(
     new ValidationPipe({
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -12,33 +12,38 @@ import { MoviesService } from './movies.service';
 import { Movie } from './entities/moive.entity';
 import { CreateMovieDTO } from './dto/create-movie.dto';
 import { UpdateMovieDTO } from './dto/update-movie.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('movies')
+@ApiTags('영화 API')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
   @Get()
+  @ApiOperation({ summary: '영화 목록 API' })
   getAll(): Movie[] {
     return this.moviesService.getAll();
   }
 
   @Get('/:id')
+  @ApiOperation({ summary: '영화 1개 조회 API' })
   getOne(@Param('id') movieId: number): Movie {
-    console.log(typeof movieId);
-
     return this.moviesService.getOne(movieId);
   }
 
   @Post()
+  @ApiOperation({ summary: '영화 생성 API' })
   create(@Body() movieData: CreateMovieDTO) {
     return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
+  @ApiOperation({ summary: '영화 삭제 API' })
   remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
   @Patch('/:id')
+  @ApiOperation({ summary: '영화 수정 API' })
   path(@Param('id') movieId: number, @Body() updateData: UpdateMovieDTO) {
     return this.moviesService.update(movieId, updateData);
   }
