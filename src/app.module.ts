1232f0be74788605d8fdaeb23c360a30282888c4
suhyeon <suhyeon3484@naver.com>
2023-06-04 17:10:11 +0900

    chore: 서비스, 컨트롤러 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 // 데코레이터 : 클래스에 함수 기능을 추가할 수 있음
 // 클래스 위 함수(아이스크림 위 초콜릿같은)
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
