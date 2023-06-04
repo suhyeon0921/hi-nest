4d1a8e94ef0c62379de38eedc2e59ddd8b22827c
suhyeon <suhyeon3484@naver.com>
2023-06-04 17:08:49 +0900

    feat: controller, service 사용

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/hello')
+  sayHelllo(): string {
+    return this.appService.getHi();
+  }
 }
