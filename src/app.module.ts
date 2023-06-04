b91a1b57c56fe5888e7f9177a87d385a7d0f3a08
suhyeon <suhyeon3484@naver.com>
2023-06-04 17:50:11 +0900

    feat: movie controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MoviesController } from './movies/movies.controller';
 
 // 데코레이터 : 클래스에 함수 기능을 추가할 수 있음
 // 클래스 위 함수(아이스크림 위 초콜릿같은)
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [MoviesController],
   providers: [],
 })
 export class AppModule {}
