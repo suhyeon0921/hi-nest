7ff6eb9a13440b98f067ff75c10b63901e472457
suhyeon <suhyeon3484@naver.com>
2023-11-10 19:52:59 +0900

    test: moive getAll and getOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
 
+  // 테스트하기 전에 실행됨
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
@@ -14,5 +16,37 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      // result가 배열인지 확인
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      // 테스트를 위해 데이터를 먼저 생성
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined(); // movie 정의 여부 확인
+      expect(movie.id).toEqual(1); // id가 1인지 확인
+    });
+
+    // 예외처리가 발생하는 지 확인
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException); // 예외처리
+        expect(e.message).toEqual('Movie with Id 999 not found'); // 메시지
+      }
+    });
   });
 });
